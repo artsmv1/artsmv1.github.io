65d1bf212f97fd0994acbbc4eb68be44f3051fb8
qq <q@q.q>
2025-07-02 18:55:18 +0300

    7 commit

--- lampa-plugins/remove-ad/remove-ad.js
+++ lampa-plugins/remove-ad/remove-ad.js
@@ -3,7 +3,7 @@
 
     var manifest = {
         type: "other",
-        version: "0.0.5",
+        version: "0.0.7",
         author: "@artsmv",
         name: "Удаление заставки",
         desc: "Плагин для удаление заставки перед показом",
@@ -11,42 +11,27 @@
     };
 
 
-    var updateplugins = false;
-    var plugins = Lampa.Storage.get('plugins', '[]')
-    plugins.forEach(function (plug) {
-        if (plug.url.indexOf('artsmv1.github.io') >= 0) {
-            updateplugins = true;
-            plug.author = manifest.author;
-            plug.desc = manifest.desc;
-            plug.description = manifest.desc;
-            plug.name = manifest.name;
-            plug.version = manifest.version;
-        }
-    })
-    if (updateplugins)
-        Lampa.Storage.set('plugins', plugins);
-
-
     function startPlugin() {
         //console.log('RAd: startPlugin(): 1');
         if (window.lampa_plugin_remove_ad) return;
-        //console.log('RAd: startPlugin(): 2');
 
         window.lampa_plugin_remove_ad = true;
+
         //Lampa.Manifest.plugins = manifest;
-
-        // not used currently
-        // Lampa.Timeline.listener.follow('view', function (e) {
-        //   console.log('Stats', 'view', e);
-        // });
-
-        // Lampa.Player.listener.follow('start', function (e) {
-        //   console.log('Stats', 'player start', e);
-        // });
-
-        // Lampa.Player.listener.follow('destroy', function (e) {
-        //   console.log('Stats', 'player destroy', e);
-        // });
+        var updateplugins = false;
+        var plugins = Lampa.Storage.get('plugins', '[]')
+        plugins.forEach(function (plug) {
+            if (plug.url.indexOf('artsmv1.github.io') >= 0) {
+                updateplugins = true;
+                plug.author = manifest.author;
+                plug.desc = manifest.desc;
+                plug.description = manifest.desc;
+                plug.name = manifest.name;
+                plug.version = manifest.version;
+            }
+        })
+        if (updateplugins)
+            Lampa.Storage.set('plugins', plugins);
 
         Lampa.Storage.listener.follow("change", function (e) {
             //console.log('RAd: Storage 1 event =', e);
@@ -55,40 +40,7 @@
                 Lampa.Storage.set("vast_plugin_launch", 1);
             }
         });
-
-        // *** MOVIES WATCHED ***
-
-        // monitor movies watched
-        // 1 - store movie data when movie card is shown
-        Lampa.Listener.follow("full", function (e) {
-            //if (e.type == "complite") {
-            //    console.log("Stats", "full complite", e);
-            //}
-        });
-
     }
-
-
-
-    // *** MENU ***
-    /*
-    console.log('Stats', 'Starting to create menu elements...');
-    
-    Lampa.SettingsApi.addComponent({
-        component: "stats",
-        icon: pluginSVG,
-        name: "Статистика",
-    });
-    */
-    // TEMP - doesn't work
-    // setTimeout(() => {
-    //   var parentContainer = document.querySelector('.settings__body .scroll__body > div');
-    //   var statsElement = document.querySelector('.settings__body .settings-folder[data-component="stats"]');
-    //   parentContainer.insertBefore(statsElement, parentContainer.firstChild);
-    // }, 2000);
-
-
-
 
 
     startPlugin();
